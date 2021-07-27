0829b56db7272402f5112961a7ca907cb242e09b
nelsinofrancisco <nelsino.francisco@gmail.com>
2021-07-27 13:51:10 -0300

    Added a id links to the correct part of page and finished the close-menu functionality

--- portfolio.js
+++ portfolio.js
@@ -1,11 +1,18 @@
 const navList = document.querySelector('.navList');
 const closeMenu = document.querySelector('.closeMenu');
 const openMenu = document.querySelector('.openMenu');
+const closeMenuLiItems = document.getElementsByClassName('links');
 
 openMenu.addEventListener('click', () => {
   navList.classList.add('active');
 });
 
 closeMenu.addEventListener('click', () => {
-  navList.classList.remove('active');
-});
+    navList.classList.remove('active');
+})
+
+for (let i = 0; i < closeMenuLiItems.length; i++) {
+  closeMenuLiItems[i].addEventListener('click', () => {
+    navList.classList.remove('active');
+  });
+}
