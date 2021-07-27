405e4a29ff1ce40dc6b35111e4495b8f4dd8c90d
nelsinofrancisco <nelsino.francisco@gmail.com>
2021-07-27 13:03:53 -0300

    Added eventlisteners for poping and closing the dropdown menu

--- portfolio.js
+++ portfolio.js
@@ -2,3 +2,10 @@ const navList = document.querySelector('.navList');
 const closeMenu = document.querySelector('.closeMenu');
 const openMenu = document.querySelector('.openMenu');
 
+openMenu.addEventListener('click', () => {
+  navList.classList.add('active');
+});
+
+closeMenu.addEventListener('click', () => {
+  navList.classList.remove('active');
+});
