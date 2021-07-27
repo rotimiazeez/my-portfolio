57b4c2442122842833841152cd0935072ba7998c
nelsinofrancisco <nelsino.francisco@gmail.com>
2021-07-27 13:55:02 -0300

    Changed JS code so it can be more Dry

--- portfolio.js
+++ portfolio.js
@@ -1,18 +1,13 @@
 const navList = document.querySelector('.navList');
-const closeMenu = document.querySelector('.closeMenu');
+const closeMenu = document.querySelector('.navList').childNodes;
 const openMenu = document.querySelector('.openMenu');
-const closeMenuLiItems = document.getElementsByClassName('links');
 
 openMenu.addEventListener('click', () => {
   navList.classList.add('active');
 });
 
-closeMenu.addEventListener('click', () => {
-    navList.classList.remove('active');
-})
-
-for (let i = 0; i < closeMenuLiItems.length; i++) {
-  closeMenuLiItems[i].addEventListener('click', () => {
+for (let i = 0; i < closeMenu.length; i++) {
+  closeMenu[i].addEventListener('click', () => {
     navList.classList.remove('active');
   });
 }
